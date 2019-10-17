da7cdb47b2a9b30e4c3a68dba0b0c7c309e40f29
JackSnowdon <jacksnowdondrums@gmail.com>
2019-10-17 15:54:31 +0000

    Added combat app and simple navbar

--- dnd/urls.py
+++ dnd/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from home import urls as urls_home
+from django.urls import path, include, re_path
 from home.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('', include(urls_home)),
+    path('home/', include('home.urls')),
+    path('combat/', include('combat.urls')),
 ]
