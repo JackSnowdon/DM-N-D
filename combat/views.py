d18d68f48fc3b2c82ff9fb468752d1044d19489b
JackSnowdon <jacksnowdondrums@gmail.com>
2019-10-22 12:46:48 +0000

    Added edit functions while in combat

--- combat/views.py
+++ combat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from itertools import chain
 from .models import * 
 from .forms import *
@@ -44,13 +44,25 @@ def delete_monster_int(request, pk=id):
     instance.delete()
     return redirect(reverse('combat_home'))
     
-def delete_hero_combat(request, pk=id):
-    print(pk)
-    players = Base.objects.all()
-    monsters = EnemyBase.objects.all()
-    matches = list(chain(players, monsters))
-    for m in matches:
-        obj=m.id
-        matches.remove()
-    return redirect(reverse('combat_home'))
+def edit_hero(request, pk):
+    hero = get_object_or_404(Player, pk=pk)
+    if request.method == "POST":
+        hero_form = AddPlayerForm(request.POST, instance=hero)
+        if hero_form.is_valid():
+            hero_form.save()
+            return redirect('combat_home')
+    else:
+        hero_form = AddPlayerForm(instance=hero)
+    return render(request, 'edit_hero.html', {'hero_form': hero_form})
+    
+def edit_monster(request, pk):
+    enemy = get_object_or_404(Monster, pk=pk)
+    if request.method == "POST":
+        monster_form = AddMonsterForm(request.POST, instance=enemy)
+        if monster_form.is_valid():
+            monster_form.save()
+            return redirect('combat_home')
+    else:
+        monster_form = AddMonsterForm(instance=enemy)
+    return render(request, 'edit_monster.html', {'monster_form': monster_form})
     
