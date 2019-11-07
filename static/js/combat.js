dc452c90356e8f376ffe539b5ddd8cbffea821ba
JackSnowdon <jacksnowdondrums@gmail.com>
2019-11-07 17:36:55 +0000

    Started combatflow using jquery

--- static/js/combat.js
+++ static/js/combat.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
     $("#start-combat").click(function() {
+
+        var index = $('.card:first').index('.card:first') + 1;
+
+        console.log(index);
+
         $("#start-combat").hide();
         $("#delete-all-combat").hide();
         $("#add-hero").hide();
@@ -8,11 +13,23 @@ $(document).ready(function() {
             $("#next-button").fadeIn("slow");
             $(".card:first").addClass("select-box");
         }, 1000);
+
+        $("#next-button").click(function() {
+            if (index == 1) {
+                $(".card:first").removeClass("select-box")
+                console.log("Dick");
+            }
+            else {
+                $(".card").eq(index).removeClass("select-box");
+                console.log("test!");
+            }
+            var first = $(".card:first")
+
+            setTimeout(function() {
+                $(".card").eq(index).addClass("select-box");
+                index++;
+                console.log(index);
+            }, 1000);
+        });
     });
-    
-    $("#next-button").click(function() {
-        $(".card:first").removeClass("select-box");
-    });
-    
-    
 });
