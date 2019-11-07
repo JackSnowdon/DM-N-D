e1fa93aabb0f7d51835c1466675a5e1b0bdad346
JackSnowdon <jacksnowdondrums@gmail.com>
2019-11-07 16:24:04 +0000

    started to write jquery for gameflow

--- static/js/combat.js
+++ static/js/combat.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
+
+    $("#start-combat").click(function() {
+        $("#start-combat").hide();
+        $("#delete-all-combat").hide();
+        $("#add-hero").hide();
+        setTimeout(function() {
+            $("#next-button").fadeIn("slow");
+            $(".card:first").addClass("select-box");
+        }, 1000);
+    });
     
+    $("#next-button").click(function() {
+        $(".card:first").removeClass("select-box");
+    });
     
     
 });
