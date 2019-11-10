3d3f37a87e7170764dec1cc3fd821743ea5c5c23
JackSnowdon <jacksnowdondrums@gmail.com>
2019-11-10 17:02:06 +0000

    added function to get name of selected card

--- static/js/combat.js
+++ static/js/combat.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var decksize, index, repeat;
+    var decksize, index, repeat, currentcard;
 
     function getDeckSize() {
         return $(".card").length;
@@ -10,7 +10,14 @@ $(document).ready(function() {
         return $(".card").index(".card") + 1;
     }
 
+    function getCurrentCard() {
+        return $(".card.select-box");
+    }
+
     $("#start-combat").click(function() {
+        $("#hide-combat").fadeIn("slow");
+
+
         repeat = 0;
         decksize = getDeckSize();
         index = getIndex();
@@ -21,26 +28,41 @@ $(document).ready(function() {
         setTimeout(function() {
             $("#next-button").fadeIn("slow");
             $(".card:first").addClass("select-box");
+            currentcard = getCurrentCard();
+            console.log(currentcard);
+
+            var name = $(currentcard).find(".card-title").text();
+
+            console.log(name);
+
         }, 1000);
+
 
     });
 
 
     $("#next-button").click(function() {
-        
+        $("#next-button").attr("disabled", true);
+
         if (repeat > 0) {
             index = 1;
             repeat--;
         }
-        
+
         var removecard = $(".card.select-box:first");
         removecard.removeClass("select-box");
 
         if (decksize > index) {
             setTimeout(function() {
                 var currentcard = $(".card").eq(index);
-                console.log(currentcard);
                 currentcard.addClass("select-box");
+                $("#next-button").attr("disabled", false);
+                console.log(currentcard);
+
+                var name = $(currentcard).find(".card-title").text();
+
+                console.log(name);
+
                 index++;
             }, 1000);
         }
@@ -48,6 +70,7 @@ $(document).ready(function() {
             $("#next-button").fadeOut("slow");
             setTimeout(function() {
                 $("#next-round").show();
+                $("#next-button").attr("disabled", false);
             }, 1000);
         }
     });
@@ -60,9 +83,9 @@ $(document).ready(function() {
             $("#next-button").fadeIn("slow");
             $(".card:first").addClass("select-box");
         }, 1000);
+        currentcard = $(".card").eq(index - 1);
+        console.log(currentcard);
         repeat++;
     });
-
-
 
 });
