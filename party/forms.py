58e43bc1b9e02a7ac089bd2d10f6744c58f36895
JackSnowdon <jacksnowdondrums@gmail.com>
2019-12-16 17:44:08 +0000

    Weapon updates

--- party/forms.py
+++ party/forms.py
@@ -6,7 +6,7 @@ class PlayerForm(forms.ModelForm):
 
     class Meta:
         model = Base
-        exclude = ['alignment', 'weapons']
+        exclude = ['alignment']
         labels = {
             "max_hp": "Max HP",
             "dex": "Dexterity",
@@ -18,7 +18,7 @@ class EnemyForm(forms.ModelForm):
 
     class Meta:
         model = Base
-        exclude = ['alignment', 'weapons']
+        exclude = ['alignment']
         labels = {
             "max_hp": "Max HP",
             "dex": "Dexterity",
