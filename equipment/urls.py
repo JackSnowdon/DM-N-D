d3082f40ba1610aecc50ded713cc209a2dfbb6b8
JackSnowdon <jacksnowdondrums@gmail.com>
2019-12-17 14:41:06 +0000

    Created add_weapon and starting to work on creating dies for weapons

--- equipment/urls.py
+++ equipment/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path(r'', equipment_home, name="equipment_home"),
+    path('add_weapon/', add_weapon, name="add_weapon"),
 ]
--- equipment/views.py
+++ equipment/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Weapon
+from .forms import *
 
 # Create your views here.
 
 def equipment_home(request):
     weapons = Weapon.objects.order_by('name')
     return render(request, 'equip_home.html', {"weapons": weapons})
+
+def add_weapon(request):
+    if request.method == "POST":
+        wepaon_form = AddWeapon(request.POST)
+        if wepaon_form.is_valid():
+            weapon_form.save()
+            messages.error(request, 'Added {0}'.format(wepaon_form.name), extra_tags='alert boldest')
+            return redirect('equipment_home')
+    else:
+        wepaon_form = AddWeapon()
+    return render(request, 'add_weapon.html', {'wepaon_form': wepaon_form})
