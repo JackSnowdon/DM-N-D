e25998e3f1a433c57bcb3b135c156945d33c49e3
JackSnowdon <jacksnowdondrums@gmail.com>
2020-01-12 13:26:13 +0000

    UX updates, model on combat home for temp combat use

--- dnd/settings.py
+++ dnd/settings.py
@@ -37,7 +37,7 @@ else:
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+       }
     }
 
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
